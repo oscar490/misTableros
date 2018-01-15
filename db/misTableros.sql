356c2b55d3e50fc6e010aa2ddbc003bddccad802
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-01-15 14:50:19 +0100

    Se ha mejorado la bd

--- db/misTableros.sql
+++ db/misTableros.sql
@@ -1,14 +1,33 @@
 
 
-DROP TABLE IF EXISTS tableros;
+DROP TABLE IF EXISTS equipos CASCADE;
+
+CREATE TABLE equipos
+(
+      id         BIGSERIAL    PRIMARY KEY
+    , nombre     VARCHAR(255) NOT NULL
+
+);
+
+
+DROP TABLE IF EXISTS tableros CASCADE;
 
 CREATE TABLE tableros
 (
 
-      id     BIGSERIAL PRIMARY KEY
+      id     BIGSERIAL    PRIMARY KEY
     , nombre VARCHAR(255) NOT NULL
+    , equipo_id BIGINT       REFERENCES equipos (id)
+                              ON DELETE CASCADE
+                              ON UPDATE CASCADE
 
 );
 
-INSERT INTO tableros (nombre)
-    VALUES ('Bienvenido');
+INSERT INTO equipos (nombre)
+    VALUES ('Tableros personales'),
+            ('2º DAW');
+
+INSERT INTO tableros (nombre, equipo_id)
+    VALUES ('Bienvenido', 1),
+            ('Programación', 2),
+            ('Autoevaluación', 2);
